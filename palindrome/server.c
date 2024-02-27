e0cca8e4445b17bbb6cc5a6a9ec185483ed6b3d1
AnsifDev <ansifofficialacc@gmail.com>
2024-02-27 22:23:11 +0530

    update

---
+++ palindrome/server.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+
+int is_palindrome(char *str) {
+    int slen  = strlen(str);
+    for (int i = 0; i < slen/2; i++) 
+        if (str[i] != str[slen-1-i]) return 0;
+    return 1;
+}
+
+int main() {
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(3000);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    printf("Creating Socket...\n");
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) { printf("\e[1A\e[2KCreating Socket...\t\t[FAIL]\n"); return 1; }
+    else printf("\e[1A\e[2KCreating Socket...\t\t[OK]\n");
+
+    printf("Binding Socket...\n");
+    int b = bind(s, (struct sockaddr*) &server, sizeof(server));
+    if (b < 0) { printf("\e[1A\e[2KBinding Socket...\t\t[FAIL]\n"); return 1; }
+    else printf("\e[1A\e[2KBinding Socket...\t\t[OK]\n");
+    
+    int l = listen(s, 5);
+    if (l < 0) { printf("ERR: Server Listening Failed\n"); return 1; }
+
+    int run = 1;
+    while (run) {
+        printf("Waiting for a connection...\n");
+        socklen_t socketlen = sizeof(server);
+        int ns = accept(s, (struct sockaddr*) &server, &socketlen);
+        if (ns < 0) { printf("Connection to client Failed\n"); return 1; }
+        else printf("New Client Connected\n");
+
+        while (1) {
+            char read_buff[100], write_buff[100];
+
+            int r = read(ns, read_buff, 100);
+            if (r == 0) break;
+            if (strcmp("SHUTDOWN", read_buff) == 0) { run = 0; break; }
+            
+            int is_pal = is_palindrome(read_buff);
+            sprintf(write_buff, "%s is%sa palindrome", read_buff, is_pal? " ": " not ");
+
+            write(ns, write_buff, strlen(write_buff));
+            printf("  %s\n", write_buff);
+        }
+
+        printf("Client Disconnected\n");
+    }
+    printf("Server Shutdown\n");
+
+    return 0;
+}
